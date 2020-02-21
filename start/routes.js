77551cf3d25e5295b27a0e6d9cf779575d0d2db9
Lucas Silveira <lugano1995@gmail.com>
2020-02-21 14:35:30 -0300

    feature: mail forgot password and update password with token

--- start/routes.js
+++ start/routes.js
@@ -6,3 +6,5 @@ const Route = use('Route')
 Route.post('users', 'UserController.store').validator('User')
 Route.post('sessions', 'SessionController.store')
 Route.post('forgot_password', 'ForgotPasswordController.store')
+
+Route.put('forgot_password', 'ForgotPasswordController.update')
